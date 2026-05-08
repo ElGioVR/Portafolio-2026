8c8f10e7609abd2a15131fb906ec6cd329ed9ad8
Giovanni Vazquez <giovanniantoniovazquezrangel@gmail.com>
2026-05-07 20:29:13 -0700

    Add opengraph.githubassets.com to Next.js images domains

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['g-global.com'], // Agrega el dominio permitido
+    domains: ['g-global.com', 'opengraph.githubassets.com'], // Agrega el dominio permitido
   }
 };
 
